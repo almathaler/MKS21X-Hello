6856c64f702c1ee84d401695a8552f8feedc5f89
almathaler <athaler00@stuy.edu>
2018-09-14 21:03:58 -0400

    Added .class and .java files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//this is a comment. This is my first CS homework of the year!
+public class HelloWorld{
+  public static void main(String[] args) {
+    System.out.println("Hello, my name is Alma Thaler. Pleased to make your acquaintance!");
+  }
+}
+//I don't really understand any of what I wrote outside of main's brackets
+//time to investigate ^^)
